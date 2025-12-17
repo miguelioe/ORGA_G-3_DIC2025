04f707c6eeb7642a582c697eac38957b66e1982a
Julio-Posadas <posadasjulionob1400@gmail.com>
2025-12-17 16:56:06 -0600

    Update main.cpp

--- practica2/code/Arduino/main.cpp
+++ practica2/code/Arduino/main.cpp
@@ -1,11 +1,10 @@
-//Incluimos librerias
-#include <Arduino.h>
+//Inlcuimos librerias
 #include <DHT.h>
 #include "Led.h"
 #include "Ldr.h"
 #include "Buzzer.h"
 
-//Definimos constantes
+// Definimos constantes
 #define DHTPIN 2
 #define DHTTYPE DHT11
 #define BUZZER 11
@@ -16,82 +15,78 @@
 
 //Creamos instancias
 DHT dht(DHTPIN, DHTTYPE);
+
 Led led_verde(LED_VERDE);
 Led led_rojo(LED_ROJO);
 Led led_amarillo(LED_AMARILLO);
+
 Ldr ldr(LDR);
 Buzzer buzzer(BUZZER);
 
-//creamos varibles
-float valor_luz;
-float valor_temperatura;
-float valor_humedad;
+// Creamos variables
+float valor_luz = 0;
+float valor_temperatura = 0;
+float valor_humedad = 0;
 
-/**
- * Inicializa todo
- */
-void setup(){
+// Setup
+void setup() {
+  Serial.begin(9600);
+
   dht.begin();
   led_verde.init();
   led_rojo.init();
   led_amarillo.init();
   buzzer.init();
-  Serial.begin(9600);
 }
-/**
- * Apaga cada uno de los leds
- */
-void apagarLeds(){
+
+// Apaga leds
+
+void apagarLeds() {
   led_verde.off();
   led_amarillo.off();
   led_rojo.off();
 }
-/**
- * Envia los datos por medio del monitor serial
- */
-void guardarDatos(float luz, float humedad, float temperatura){
+
+// Envía datos a processing
+void enviarDatos(float luz, float humedad, float temperatura) {
   Serial.print(temperatura);
   Serial.print(",");
   Serial.print(humedad);
   Serial.print(",");
   Serial.println(luz);
-  
 }
 
-/**
- * Enciende y apaga los leds y el buzzer dependiendo de la temperatura
- */
-void cambioTemperatura(){
+void cambioTemperatura() {
   apagarLeds();
-  if(valor_temperatura < 25){
+  buzzer.off();
+
+  if (valor_temperatura < 25) {
     led_verde.on();
   }
-  else if(valor_temperatura > 30){
+  else if (valor_temperatura > 30) {
     led_rojo.on();
     buzzer.on();
   }
-  else{
+  else {
     led_amarillo.on();
   }
 }
-/**
- * Método principal, lee datos, los guarda y hace los cambios de los leds
- */
-void loop(){
+
+// Loop
+void loop() {
 
   valor_luz = ldr.read();
   valor_temperatura = dht.readTemperature();
   valor_humedad = dht.readHumidity();
 
-  if(isnan(valor_humedad) || isnan(valor_temperatura)){
-    Serial.println("Error al momento de leer el componente");
+  // Si hay error, NO enviamos nada
+  if (isnan(valor_humedad) || isnan(valor_temperatura)) {
     return;
   }
 
-  guardarDatos(valor_luz, valor_humedad, valor_temperatura);
+  enviarDatos(valor_luz, valor_humedad, valor_temperatura);
   cambioTemperatura();
 
   delay(3000);
-
 }
 
